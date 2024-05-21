93b36fdf826bf20e7e39840efcc415639f2aa945
zezeon <jjaewon527@gmail.com>
2024-05-21 20:41:26 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,12 @@ public class Main {
         int param = sc.nextInt();
 
 
-        Calculable expression = new MultiplyExpression();
-        Calculable expression = new AddExpression();
+        Calculable expression1 = new MultiplyExpression();
+        Calculable expression2 = new AddExpression();
 
-        System.out.printf("my expression's result : %d \n", expression.calculate(param));
+        System.out.printf("my multiply expression's result : %d \n", expression1.calculate(param));
+        System.out.printf("my add expression's result : %d \n", expression2.calculate(param));
+
     }
 }
 
@@ -21,13 +23,17 @@ interface Calculable {
 }
 
 //본인이 작성한 코드를 붙여넣어주세요!
-class MultiplyExpression implements Calculable{
+class MultiplyExpression implements Calculable {
     @Override
     public int calculate(int param) {
-        return param*9;
-class AddExpression implements Calculable{
-    @Override
-    public int calculate(int param) {
-        return param+1;
+        return param * 9;
+
     }
 }
+    class AddExpression implements Calculable {
+        @Override
+        public int calculate(int param) {
+            return param + 1;
+        }
+
+    }
